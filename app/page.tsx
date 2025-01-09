48a83fd70562c634a218eb4605c0b927dc920711
Nick Alteen <ncalteen@github.com>
2025-01-09 14:19:38 -0500

    Add template

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 pt-[20%] gap-16 font-[family-name:var(--font-geist-sans)]">
       <HeroImage />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 pt-[20%] font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center">
+        <main className="flex flex-col gap-8 row-start-2 pt-16 mt-16 items-center">
           <Carousel opts={{ align: 'center' }} className="w-full max-w-lg">
             <CarouselContent>
               <CarouselItem className="md:basis-1/2 lg:basis-1/3 pl-4">
@@ -255,8 +255,7 @@ export default function Home() {
             </div>
           </div>
         </main>
-
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <footer className="row-start-3 flex gap-6 pt-8 flex-wrap items-center justify-center">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
             href="https://github.com/issue-ops/bear-creek-honey-farm"
--- components/ui/hero-image.tsx
+++ components/ui/hero-image.tsx
@@ -13,9 +13,13 @@ export default function HeroImage() {
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
         <h1 className="text-4xl font-bold">Welcome to Bear Creek Honey Farm</h1>
         <p className="mt-4 text-lg">The best in AI-generated hospitality</p>
-        <button className="mt-6 px-4 py-2 bg-yellow-500 text-black rounded">
+        <a
+          href="https://github.com/issue-ops/bear-creek-honey-farm/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-6 px-4 py-2 bg-yellow-500 text-black rounded">
           Reserve today!
-        </button>
+        </a>
       </div>
     </div>
   )
