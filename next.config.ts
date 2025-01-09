1102504b8927a1edcc31edf20ceb328ad0eb43f2
Nick Alteen <ncalteen@github.com>
2025-01-09 14:53:33 -0500

    Update config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/bear-creek-honey-farm'
+  basePath: '/bear-creek-honey-farm',
+  reactStrictMode: true
 }
 
 export default nextConfig
