458a4a046058acd5e821220ace6d70e322b521fb
Nick Alteen <ncalteen@github.com>
2025-01-09 14:28:45 -0500

    Add workflows

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: '/bear-creek-honey-farm'
 }
 
 export default nextConfig
