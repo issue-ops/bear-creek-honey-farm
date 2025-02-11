e2d636d420ddc9a1807d35ea71a4512dbffe2ac6
Nick Alteen <ncalteen@github.com>
2025-02-11 16:03:14 -0500

    Update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss'
-
 export default {
   darkMode: ['class'],
   content: [
@@ -60,4 +58,4 @@ export default {
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require('tailwindcss-animate')]
-} satisfies Config
+}
